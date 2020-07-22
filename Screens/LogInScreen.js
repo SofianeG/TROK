68fd594a37cb2cc946ac8a3fa6c1d88b87329ba4
SofianeG  <sofianegougam17@gmail.com>
2020-07-22 14:45:08 +0200

    screen fini

--- Screens/LogInScreen.js
+++ Screens/LogInScreen.js
@@ -9,49 +9,46 @@ import LoginScreenIcon from '../assets/Icons/LoginScreenIcon'
 
 
 export class LogInScreen extends Component {
+   
     render() {
         return (
-                <View style={styles.container}>
+                <View style={styles.container}> 
                     <View style={styles.wrapper_top}>
-                    <ImageBackground source={require("../assets/image_loginScreen.jpg")} style={{height:"100%"}}>
-                        <View style={{height:"100%"}}>
-                            <View style={{height:"75%",justifyContent:"center",}}>
-                                <LoginScreenIcon/>
-                            </View>
-                            <View style={{alignContent:"flex-end",height:"25%"}}>
-                                <Text style={styles.title}>Bienvenue</Text>
-                                <Text style={styles.sub_title}>L’application éco-responsable et solidaire de troc d’objets et de services entre particuliers !</Text>
-                            </View>
-                        </View>
-                    </ImageBackground>    
-                       </View>
+                        <ImageBackground source={require("../assets/image_loginScreen.jpg")} style={{height:"100%"}}>
+                                <View style={{flex:1,marginHorizontal:20, marginVertical:35,}}>
+                                    <LoginScreenIcon/>
+                                </View>
+                                <View style={{marginHorizontal:20,marginVertical:30}}>
+                                    <Text style={styles.title}>Bienvenue</Text>
+                                    <Text style={styles.sub_title}>L’application éco-responsable et solidaire de troc d’objets et de services entre particuliers !</Text>
+                                </View>
+                        </ImageBackground>
+                    </View>
                     <View style={styles.wrapper_bottom}>
-                                <View>
+                                <View >
                                     <TouchableOpacity style={styles.btn_google}>
-                                        <View style={{width:"5%",justifyContent:"flex-end"}}>
+                                        <View >
                                             <GoogleIcon/>
                                         </View>
-                                        <View style={{justifyContent:"space-around",width:"65%"}}>
                                             <Text style={styles.google_text}>Login avec Google</Text>
-                                        </View>
                                     </TouchableOpacity>
                                 </View>
-                                <View>
+                                <View >
                                     <TouchableOpacity style={styles.btn_facebook}>
-                                    <View style={{width:"5%",justifyContent:"flex-end"}}>
-                                        <FacebookIcon/>
-                                    </View>
-                                    <View style={{justifyContent:"space-around",width:"65%"}}>
-                                        <Text style={styles.facebook_text}>Login avec Facebook</Text>
-                                    </View>
+                                        <View>
+                                            <FacebookIcon/>
+                                        </View>
+                                            <Text style={styles.facebook_text}>Login avec Facebook</Text>
                                     </TouchableOpacity>
                                 </View>
                                 <View>
                                     <TouchableOpacity style={styles.btn_email}>
-                                    <Text style={styles.mail_text}>Login avec mon mail</Text>
+                                        <Text style={styles.mail_text}>Login avec mon mail</Text>
                                     </TouchableOpacity>
                                 </View>
-                                <Text style={{textDecorationLine:"underline", color:"white",fontSize:16}}>Se connecter</Text>
+                                <View style={{marginVertical:40,}}>
+                                    <Text style={{textDecorationLine:"underline", color:"white",fontSize:16}}>Se connecter</Text>
+                                </View>
                     </View>
                 </View>
 
@@ -60,18 +57,42 @@ export class LogInScreen extends Component {
 }
 
 const styles= StyleSheet.create({
-
+    container:{
+    flex:1,
+    },  
     wrapper_top: {
-        height:"45%",
-      },
+        height:396,
+    },
     wrapper_bottom: {
         backgroundColor:"#40CE6A",
-        height:"55%",
-        width:376,
-        justifyContent:"space-evenly",
+        height:416,
+        width:"100%",
+        paddingHorizontal:70,
+        paddingVertical:45,
         alignItems:"center",
         alignSelf:"center",
       },
+      title:{
+        height: 20,
+        width: 319,
+        color: "#FFFFFF",
+        // font-family: Avenir; font payante
+        fontSize: 20,
+        fontWeight:"bold",// fontWeight: 900,
+        letterSpacing: 0,
+        lineHeight: 20,
+        marginVertical:18
+    },
+    sub_title: {
+        height: 40,
+        width: 319,
+        color: "#FFFFFF",
+        // font-family: Avenir;
+        fontSize: 15,
+        // fontWeight: 500,
+        letterSpacing: 0,
+        lineHeight: 20,
+    },
     btn_google:{
         height: 52,
         borderRadius: 26,
@@ -79,8 +100,8 @@ const styles= StyleSheet.create({
         display:"flex",
         flexDirection:"row",
         alignItems:"center",
-        justifyContent:"space-evenly",
-        width:"65%",
+        paddingHorizontal:33,
+        width: 236,
     },
     btn_facebook:{
         height: 52,
@@ -89,8 +110,9 @@ const styles= StyleSheet.create({
         display:"flex",
         flexDirection:"row",
         alignItems:"center",
-        justifyContent:"space-evenly",
-        width:"65%",
+        paddingHorizontal:34,
+        width: 236,
+        marginVertical:25,
         
     },
     btn_email:{
@@ -105,40 +127,22 @@ const styles= StyleSheet.create({
     },
     google_text:{
         fontSize:16,
-        color:"#6D7278"
+        color:"#6D7278",
+        width:136,
+        height:20,
+        marginHorizontal:14,
     },
     facebook_text:{
         fontSize:16,
-        color:"#FFFFFF"
+        color:"#FFFFFF",
+        width:155,
+        height:20,
+        marginHorizontal:18,
     },
     mail_text:{
         fontSize:16,
         color:"#FFFFFF"
     },
-    title:{
-        height: 20,
-        width: 319,
-        color: "#FFFFFF",
-        // font-family: Avenir; font payante
-        fontSize: 20,
-        fontWeight:"bold",// fontWeight: 900,
-        letterSpacing: 0,
-        lineHeight: 20,
-        marginHorizontal:18,
-    },
-    sub_title: {
-        height: 40,
-        width: 319,
-        color: "#FFFFFF",
-        // font-family: Avenir;
-        fontSize: 14,
-        fontWeight:"bold",// fontWeight: 500,
-        letterSpacing: 0,
-        lineHeight: 20,
-        marginVertical:25,
-        marginHorizontal:18,
-    }
-    
 })
 
 export default LogInScreen;
