fc2329e7eabec20612016f7caa41537c3f207b20
SofianeG <sofianegougam17@gmail.com>
2020-07-29 00:01:37 +0200

    Splash Screen bientot Terminé

--- App.js
+++ App.js
@@ -1,62 +1,10 @@
-import React,{Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {} from "react-native";
 
-import SplashScreen from './Screens/SplashScreen'
-import LoginScreen from './Screens/LogInScreen'
-import AccountScreen from './Screens/AccountScreen'
-import AccountNameScreen from './Screens/AccountNameScreen'
-import AccountPictureScreen from './Screens/AccountPictureScreen'
+import SplashScreen from "./Screens/SplashScreen";
 
-import CustomButton from './Component/CustomButton/CustomButton'
-import { NavigationContainer } from '@react-navigation/native';
-
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
-
-
-const Stack = createStackNavigator();
-
-
-
-function LogInScreen(){
-  return(
-    <LogInScreen/>
-  )
+function App() {
+  return <SplashScreen />;
 }
-function AccountS(){
-  return(
-    <AccountScreen/>
-  )
-}
-function AccountNS(){
-  return(
-    <AccountNameScreen/>
-  )
-}
-
-function App(){
-
-    return (
-      <NavigationContainer>
-       <Stack.Navigator initialRouteName="Home" headerMode="none">
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen  name="AccountS"  component={AccountS} />
-          <Stack.Screen  name="AccountNS"  component={AccountNS} />
-          </Stack.Navigator>
-      </NavigationContainer>
-      
-     
-    )
-}
-
 
 export default App;
-
-// <NavigationContainer>
-      //    <Stack.Navigator initialRouteName="Home" headerMode="none">
-      //     <Stack.Screen name="Home" component={HomeScreen} />
-      //     <Stack.Screen  name="AccountS"  component={AccountS} />
-      //     <Stack.Screen  name="AccountNS"  component={AccountNS} />
-      //   </Stack.Navigator>
-      // </NavigationContainer>  
-
--- Screens/SplashScreen.js
+++ Screens/SplashScreen.js
@@ -1,34 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
 import SplashScreenIcon from "../assets/Icons/SplashScreenIcon";
 import normalize from "react-native-normalize";
 
+import Colors from "../assets/Style/Colors";
+
 function SplashScreen() {
   return (
     <View style={styles.container}>
       <View>
-        <SplashScreenIcon />
+        <SplashScreenIcon width={normalize(266)} height={normalize(99)} />
+        <Text style={styles.text}>Le troc participatif et solidaire</Text>
       </View>
-      <Text style={styles.text}>Le troc participatif et solidaire</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#40CE6A",
+    backgroundColor: Colors.main_green,
     height: normalize(812),
     width: normalize(375),
     paddingHorizontal: normalize(54),
     paddingTop: normalize(242),
   },
   text: {
-    fontSize: 21.6,
-    color: "#FFFFFF",
+    // fontFamily: Pangolin Regular
+    fontSize: normalize(21.4),
+    color: Colors.splash_screen_text,
     paddingTop: normalize(41),
-    letterSpacing: -0.24,
-    lineHeight: 28,
+    letterSpacing: -0.24, //le normalize ne passe pas ici avec une valeur negative
+    lineHeight: normalize(28),
   },
 });
 
